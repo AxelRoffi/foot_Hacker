265b55371f9dead925f67affbd60737f8073590a
Grégoire Basset <gregoirebasset@gmail.com>
2017-06-06 12:06:02 +0200

    update_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'games/index'
+
+  get 'games/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :games, only: [:index, :show]
 end
