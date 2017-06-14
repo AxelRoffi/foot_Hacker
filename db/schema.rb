db532aa37a044f008a6826e9fcb4d82d56f61f34
Axel Roffi <axel.roffi@gmail.com>
2017-06-14 14:29:17 +0200

    fix with git

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170613133937) do
+ActiveRecord::Schema.define(version: 20170614113814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +29,18 @@ ActiveRecord::Schema.define(version: 20170613133937) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
+  create_table "appearances", force: :cascade do |t|
+    t.string "position"
+    t.bigint "player_id"
+    t.bigint "team_id"
+    t.bigint "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_appearances_on_game_id"
+    t.index ["player_id"], name: "index_appearances_on_player_id"
+    t.index ["team_id"], name: "index_appearances_on_team_id"
+  end
+
   create_table "games", force: :cascade do |t|
     t.date "datetime"
     t.datetime "created_at", null: false
@@ -41,12 +52,12 @@ ActiveRecord::Schema.define(version: 20170613133937) do
   end
 
   create_table "players", force: :cascade do |t|
-    t.string "first_name"
     t.string "last_name"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "team_id"
+    t.integer "api_id"
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
@@ -94,6 +105,7 @@ ActiveRecord::Schema.define(version: 20170613133937) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "AddOmniauthToUsers"
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
@@ -104,6 +116,9 @@ ActiveRecord::Schema.define(version: 20170613133937) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appearances", "games"
+  add_foreign_key "appearances", "players"
+  add_foreign_key "appearances", "teams"
   add_foreign_key "players", "teams"
   add_foreign_key "ratings", "games"
   add_foreign_key "ratings", "teams"
