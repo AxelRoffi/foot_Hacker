41fe225d030bf2029c381f4b7ca89316b656399d
Axel Roffi <axel.roffi@gmail.com>
2017-06-14 14:03:58 +0200

    Populate db with players

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :local_team, class_name: "Team"
   belongs_to :visitor_team, class_name: "Team"
+  has_many :appearances
+
 end
---
+++ db/migrate/20170614101205_create_appearances.rb
@@ -0,0 +1,12 @@
+class CreateAppearances < ActiveRecord::Migration[5.1]
+  def change
+    create_table :appearances do |t|
+      t.string :position
+      t.references :player, foreign_key: true
+      t.references :team, foreign_key: true
+      t.references :game, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
