f3105af3b5febd7af9273ebb319f7b66031c1139
Axel Roffi <axel.roffi@gmail.com>
2017-06-15 18:01:05 +0200

    Syntax Error

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,8 @@
 class PlayersController < ApplicationController
 
-  def index
-  # @players = Player.where(local_team: current_user.profile.team)
-  #                 .or(Game.where(visitor_team: current_user.profile.team))
-  end
+#   def index
+#   # @players = Player.where(local_team: current_user.profile.team)
+#   #                 .or(Game.where(visitor_team: current_user.profile.team))
+#   end
+
+end
